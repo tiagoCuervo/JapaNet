dfecd761220864be921ca16bf45789c96d330f38
tiagoCuervo <dscuervog@gmail.com>
2021-03-15 12:05:29 +0100

    Added uncompressing and initial EDA

--- scripts/download_dataset.py
+++ scripts/download_dataset.py
@@ -1,15 +1,18 @@
 import requests
+import os
+from pathlib import Path
 
-def download_file_from_google_drive(id, destination):
+
+def download_file_from_google_drive(gdriveID, destination):
     URL = "https://docs.google.com/uc?export=download"
 
     session = requests.Session()
 
-    response = session.get(URL, params={'id': id}, stream=True)
+    response = session.get(URL, params={'id': gdriveID}, stream=True)
     token = get_confirm_token(response)
 
     if token:
-        params = {'id': id, 'confirm': token}
+        params = {'id': gdriveID, 'confirm': token}
         response = session.get(URL, params=params, stream=True)
 
     save_response_content(response, destination)
@@ -33,21 +36,26 @@ def save_response_content(response, destination):
 
 
 if __name__ == "__main__":
-
-    outputTrainCsv              = "./data/train.csv"
-    outputUnicodeTranslation    = "./data/unicode_translation.csv"
-    outputTrainImages           = "./data/train_images.zip"
-    outputTestImages            = "./data/test_images.zip"
+    outputTrainCsv = "../data/train.csv"
+    outputUnicodeTranslation = "../data/unicode_translation.csv"
+    outputTrainImages = "../data/train_images.zip"
+    outputTestImages = "../data/test_images.zip"
     outputs = [outputTrainCsv, outputUnicodeTranslation, outputTrainImages, outputTestImages]
 
-    googleFileID_trainCSV                   = "1QkUqqFG3mOVvyvoOcOP3uaYmfgrLTxcU"
-    googleFileID_outputUnicodeTranslation   = "1DIHqCuIgDcJkrik4YT1GagHzZ_rMtBPc"
-    googleFileID_trainImages                = "1lyYZh5LwQUJCxCZ2EEqXPkQ5r1Gq3LzJ"
-    googleFileID_testImages                 = "1Y8JgczT1chZY2DxLbMD2VF5emTbAYRCS"
-    googleFilesID = [googleFileID_trainCSV, googleFileID_outputUnicodeTranslation
-                     , googleFileID_trainImages, googleFileID_testImages]
+    googleFileID_trainCSV = "1QkUqqFG3mOVvyvoOcOP3uaYmfgrLTxcU"
+    googleFileID_outputUnicodeTranslation = "1DIHqCuIgDcJkrik4YT1GagHzZ_rMtBPc"
+    googleFileID_trainImages = "1lyYZh5LwQUJCxCZ2EEqXPkQ5r1Gq3LzJ"
+    googleFileID_testImages = "1Y8JgczT1chZY2DxLbMD2VF5emTbAYRCS"
+    googleFilesID = [googleFileID_trainCSV, googleFileID_outputUnicodeTranslation, googleFileID_trainImages,
+                     googleFileID_testImages]
 
     for out, fileID in zip(outputs, googleFilesID):
         download_file_from_google_drive(fileID, out)
     print('Download Complete!')
-
+    print('Uncompressing files ...')
+    dataDir = os.path.join(Path(__file__).parent.parent, 'data')
+    os.system(f"unzip \"{os.path.join(dataDir, 'train_images.zip')}\" -d \"{os.path.join(dataDir, 'train')}\"")
+    os.system(f"unzip \"{os.path.join(dataDir, 'test_images.zip')}\" -d \"{os.path.join(dataDir, 'test')}\"")
+    os.system(f"rm \"{os.path.join(dataDir, 'train_images.zip')}\"")
+    os.system(f"rm \"{os.path.join(dataDir, 'test_images.zip')}\"")
+    print('All done! :D')
