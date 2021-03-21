43d1c3d2bec68428016d40ce0753cd81bf7deecf
tiagoCuervo <dscuervog@gmail.com>
2021-03-21 18:05:47 +0100

    Modified data loader

---
+++ config/default.py
@@ -0,0 +1,10 @@
+datasetParams = {
+    'shufflingBufferSize': 100,
+    'batchSize': 32,
+    'identifierInputHeight': 512,
+    'identifierInputWidth': 512,
+    'identifierOutputStride': 4,
+    'validationFraction': 0.2,
+    'classifierInputWidth': 64,
+    'classifierInputHeight': 64
+}
