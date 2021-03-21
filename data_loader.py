e10c5ba823161afb2af8a2789c4e3cbbb3013a7d
Rodrigo Cofré <rlcofre@uc.cl>
2021-03-21 14:13:40 +0100

    Update data_loader.py
    Added Classifier dataset loading

--- data_loader.py
+++ data_loader.py
@@ -4,6 +4,8 @@ import numpy as np
 import tensorflow as tf
 import importlib
 from tqdm.auto import tqdm
+import os
+from pathlib import Path
 
 
 def _addIdentificationLabels(df):
@@ -14,6 +16,20 @@ def _addIdentificationLabels(df):
         pageData[:, 0] += pageData[:, 2] // 2  # Center on X
         pageData[:, 1] += pageData[:, 3] // 2  # Center on Y
         df.loc[i, "labels"] = pageData[:, :2].ravel()
+    return df
+
+
+def _addClassificationLabels(imgs_folder_path):
+    pair_img_label = []
+    for CLASS in os.listdir(imgs_folder_path):
+        if CLASS =='train_char.zip':
+            continue
+        else:
+            class_dir = os.path.join(folders_dir, CLASS)
+            for img_id in os.listdir(class_dir):
+                pair_img_label.append([img_id.split('.')[0], CLASS])
+    
+    df = pd.DataFrame(pair_img_label, columns=['image_id','label'])
     return df
 
 
@@ -70,6 +86,55 @@ class IdentifierDataset:
         self.writer.close()
 
 
+class ClassifierDataset:
+    def __init__(self, config):
+        self.config = config
+        self.recordPath = 'train/train_classifier.tfrecord'
+        self.writer = tf.io.TFRecordWriter(self.recordPath)
+        self.feature_description = {
+            'image': tf.io.FixedLenFeature([], tf.string),
+            'label': tf.io.VarLenFeature(dtype=tf.string)
+        }
+
+    def _write(self, image, label):
+        feature = {
+            'image': _bytesFeature(image),
+            'label': _bytresFeature(label)
+        }
+        sample = tf.train.Example(features=tf.train.Features(feature=feature))
+        self.writer.write(sample.SerializeToString())
+
+    def _processExample(self, example):
+        pmap = tf.io.parse_single_example(example, self.feature_description)
+        imageDecoded = tf.image.decode_jpeg(pmap['image'], channels=3) / 255
+        imageResized = tf.image.resize(imageDecoded, [self.config.identifierInputWidth,
+                                                      self.config.identifierInputHeight])
+        # label = tf.sparse.to_dense(pmap['labels'])
+        # return imageResized, label
+        return imageResized
+
+    def _read(self):
+        record = tf.data.TFRecordDataset(self.recordPath)
+        trainData = record.map(
+            self._processExample, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        trainData = trainData.shuffle(buffer_size=100)
+        trainData = trainData.batch(self.config.batchSize, drop_remainder=True)
+        trainData = trainData.prefetch(
+            buffer_size=tf.data.experimental.AUTOTUNE)
+        return trainData
+
+    def createDataset(self):
+        train_char_dir = Path("data/train_char")
+        dfTrain = _addClassificationLabels(train_char_dir)
+        for i in tqdm(range(len(dfTrain))):
+            sample = dfTrain.iloc[i]
+            self._write(
+                open("data/train_char/"+sample['label'] + "/" + sample['image_id'] + ".jpg", 'rb').read(), sample['label'])
+        self.writer.flush()
+        self.writer.close()
+
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -77,10 +142,15 @@ if __name__ == '__main__':
                         help='config file name')
     parser.add_argument('--identifier', dest='identifier', action='store_true')
     parser.set_defaults(identifier=False)
+
     args = parser.parse_args()
 
     config = importlib.import_module(f"config.{args.config}")
 
     if args.identifier:
         dataset = IdentifierDataset(config.datasetParams)
+        dataset.createDataset()
+
+    if not arg.identifier:
+        dataset = ClassifierDataset(config.datasetParams)
         dataset.createDataset()
